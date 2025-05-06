929eb7245ab720d2f5457200de3616516927e22c
Ilya <Stepanenko.my-mail@yandex.ru>
2025-05-06 17:02:21 +1100

    Небольшие доработки страницы пользователей

--- Interface/scripts/users.js
+++ Interface/scripts/users.js
@@ -1,15 +1,26 @@
 const manageUsers = document.getElementById('manegeUsers');
 
 let dataUsers = data.dataUsers()
-console.log(dataUsers)
+let objectUsers = JSON.parse(dataUsers)
+let mapUsers = objectUsers.Users.map( elem => `
+        <tr>
+                <td>${elem.LastName} ${elem.FirstName} ${elem.Patronymic}</td>
+                <td>${elem.Nickname}</td>
+                <td>${elem.password}</td>
+                <td>${elem.rolle}</td>
+        </tr>`)
+
+const createUsersString = function (usersArray) {
+        let users = "";
+        usersArray.forEach(element => {
+                users +=  element
+        });
+        return users;
+}
 
 let usersTableCaptions = `<tr><th>Ф.И.О.</th><th>Nickname</th><th>Password</th><th>Rolle</th></tr>`
-let usersTableItems = `
-        <tr><td>Stephen C. Cox</td><td>$300</td><td>$50</td><td>Bob</td></tr>
-        <tr><td>Josephin Tan</td><td>$150</td><td>-</td><td>Annie</td></tr>
-        <tr><td>Joyce Ming</td><td>$200</td><td>$35</td><td>Andy</td></tr>
-        <tr><td>James A. Pentel</td><td>$175</td><td>$25</td><td>Annie</td></tr>
-        `
-let usersTable = usersTableCaptions + usersTableItems;
+let usersString = createUsersString(mapUsers)
+
+let usersTable = usersTableCaptions + usersString;
 
 manageUsers.innerHTML = usersTable;
