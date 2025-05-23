6d8f2dd3c2fbcd2d9fd1416d741b9e305f733d20
Ilya <Stepanenko.my-mail@yandex.ru>
2025-05-23 14:43:55 +1100

    Вызов модального окна по клику в интерфейсе

--- Interface/scripts/vksList.js
+++ Interface/scripts/vksList.js
@@ -1,6 +1,9 @@
 
 const VKSList_btnCreateVKS = document.getElementById('VKSList_btnCreateVKS');
-
+const VKSList_link = document.querySelector('.VKSList_link_wrapper');
+const modalWindow = document.querySelector('.modal_wrapper');
+const btnModal_Edit = document.getElementById('btnModal_Edit');
+const btnModal_Close = document.getElementById('btnModal_Close');
 
 VKSList_btnCreateVKS.onclick = function () {
     window.location.replace("create-vks.html");
@@ -9,3 +12,8 @@ VKSList_btnCreateVKS.onclick = function () {
 iventsController.GetAllIvents().then( res => {
     console.log(res)
 })
+
+
+/* Включение / Выключение модального окна */
+VKSList_link.addEventListener('click', (e) => {modalWindow.style.display = 'flex';})
+btnModal_Close.onclick = function () {modalWindow.style.display = 'none';}
