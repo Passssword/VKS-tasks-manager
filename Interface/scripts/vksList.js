c51dbd838478e7a6dbe0855e491d7ebc56155dcb
Ilya <Stepanenko.my-mail@yandex.ru>
2025-05-23 16:40:44 +1100

    модальное окно вызывается по клику в интерфейсе

--- Interface/scripts/vksList.js
+++ Interface/scripts/vksList.js
@@ -1,19 +1,64 @@
 
 const VKSList_btnCreateVKS = document.getElementById('VKSList_btnCreateVKS');
-const VKSList_link = document.querySelector('.VKSList_link_wrapper');
+
 const modalWindow = document.querySelector('.modal_wrapper');
 const btnModal_Edit = document.getElementById('btnModal_Edit');
 const btnModal_Close = document.getElementById('btnModal_Close');
+const VKSList_ivent_wrapper = document.getElementById('VKSList_ivent_wrapper');
+
+
 
 VKSList_btnCreateVKS.onclick = function () {
     window.location.replace("create-vks.html");
 }
 
-iventsController.GetAllIvents().then( res => {
-    console.log(res)
+const mapIvents = (ivents) => {
+
+    return ivents.map(ivent => `
+        <a class="VKSList_link_wrapper" href="#">
+            <div class="VKSList_event_wrapper">
+
+            <div class="VKSList_event_caption">
+                <p>${ivent.iventDate}</p>
+                <p>${ivent.iventObject}</p>
+                <p>${ivent.iventType}</p>
+            </div>
+
+        </div></a>`
+    )
+}
+
+const arrToString = (array) => {
+    let stringValue = ""
+    array.forEach(element => {
+        stringValue += element
+    });
+
+    return stringValue
+}
+
+function addEventsButtons (ivents) {
+    const VKSList_link = document.querySelectorAll('.VKSList_link_wrapper');
+    
+    let openEventLinkCount = 0
+    VKSList_link.forEach( element => {
+        element.addEventListener('click', (e) => {modalWindow.style.display = 'flex';})
+    })
+}
+
+iventsController.GetAllIvents().then( ivents => {
+    console.log(ivents)
+
+    let iventsMap = mapIvents(ivents)
+    let iventsHTML = arrToString(iventsMap)
+
+    VKSList_ivent_wrapper.innerHTML = iventsHTML
+
+    addEventsButtons (ivents)
 })
 
 
+
 /* Включение / Выключение модального окна */
-VKSList_link.addEventListener('click', (e) => {modalWindow.style.display = 'flex';})
+// VKSList_link.addEventListener('click', (e) => {modalWindow.style.display = 'flex';})
 btnModal_Close.onclick = function () {modalWindow.style.display = 'none';}
